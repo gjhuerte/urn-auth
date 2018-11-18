2d1ddc30bbfe5740af080d0b4c003ad16706f1c6
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-18 20:41:18 +0800

    Added different event handling on authentication

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from '@firebase/app';
 import '@firebase/auth';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyD0rCEcQ-Mi7iXM1tzgD9VEvvDpc-E3KqI',
@@ -15,13 +17,38 @@ class App extends Component {
       storageBucket: 'authentication-38fdb.appspot.com',
       messagingSenderId: '752680039115'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button
+            onPress={() => firebase.auth().signOut()}
+          >
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
